31218831d6f087d42c066de5d76b2fbdd5ed80c0
Martin Hinsch <hinsch.martin@gmail.com>
2017-05-16 13:09:24 +0100

    more tests

--- tests/testthat/test_edgelist.R
+++ tests/testthat/test_edgelist.R
@@ -26,5 +26,20 @@ test_that("sinks are identified correctly", {
 	expect_equal(sksf, factor("3", levels=c("0", "1", "2", "3")))
 })
 
+edgelistc <- data.frame(c(0L, 1L, 2L, 3L), c(2L, 2L, 3L, 0L))
+edgelistcf <- data.frame(c("0", "1", "2", "3"), c("2", "2", "3", "0"))
 	
+test_that("cycles are found", {
+	expect_true(cycles(edgelistc))
+	expect_true(cycles(edgelistcf))
+
+	c <- cycles(edgelistc, TRUE)
+	cf <- cycles(edgelistcf, TRUE)
+
+	expect_equal(sort(c[[1]]), c(0, 2, 3))
+	expect_equal(sort(as.character(cf[[1]])), c("0", "2", "3"))
+})
+
+
+
 
