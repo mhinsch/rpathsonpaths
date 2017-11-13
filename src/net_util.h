2b48a66e44393158a58f5d0d287b7a77ba1b9fd2
Martin Hinsch <hinsch.martin@gmail.com>
2017-11-13 12:05:01 +0000

    docs

--- src/net_util.h
+++ src/net_util.h
@@ -10,45 +10,51 @@
 struct Cycles
 	{
 	const vector<vector<size_t>> & net; //!< Network as children per node.
-	vector<bool> visited;
-	vector<bool> done;
+	vector<bool> visited;				//!< Within calls (cycles).
+	vector<bool> done;					//!< Between calls (optimization).
 	vector<size_t> stack;
-	vector<vector<size_t>> res;
+	vector<vector<size_t>> res;			//! A list of cycles.
 
 	/** Plain constructor. */
 	Cycles(const vector<vector<size_t>> & network)
 		: net(network), visited(network.size(), false), done(network.size(), false)
 		{}
 
-	/** Detect if there's at least one cycle. This can be significantly faster than finding
-	 * (and returning) all cycles. */
+	/** Detect if there's at least one cycle in the subnetwork reachable from node cur. This 
+	 * can be significantly faster than tracking down all cycles. Note that calling this
+	 * directly on a non-source node might produce false positives down the line. */
 	bool has_cycles(size_t cur)
 		{
-		visited[cur] = true;
-		done[cur] = true;
+		visited[cur] = true;	// this will detect cycles
+		done[cur] = true;		// we keep track of processed nodes so we can skip them
 
+		// check children
 		for (size_t i : net[cur])
 			{
-			if (visited[i])
+			if (visited[i])	// been here => cycle!
 				return true;
 
-			if (!done[i])
+			if (!done[i])	// hasn't been processed => do it now
 				if (has_cycles(i))
 					return true;
 			}
 
-		visited[cur] = false;
+		visited[cur] = false;	// entire subtree has been checked, clean up
 		return false;
 		}
 
+	/** Find and record all cycles in the subnetwork reachable from cur. */
 	void find_cycles(size_t cur)
 		{
-		stack.push_back(cur);
+		stack.push_back(cur);	// keep track of current path
 		done[cur] = true;
 
+		// check children
 		for (size_t i : net[cur])
 			{
+			// are we crossing our own path?
 			const auto f = find(stack.begin(), stack.end(), i);
+			// if so, add the entire loop to the list of cycles
 			if (f != stack.end())
 				{
 				res.push_back(vector<size_t>(f, stack.end()));
